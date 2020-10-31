9fbe1f62a5c58e2a189680df7b1d9c3a37d08885
congzw <2981810300@qq.com>
2020-10-31 10:09:53 +0800

    remove old projects

--- src/NbSites.Web.ModuleMvc/Manifest.cs
+++
@@ -1,5 +0,0 @@
-using OrchardCore.Modules.Manifest;
-
-[assembly: Module(
-    Name = "ModuleMvc HelloWorld"
-)]
--- src/NbSites.Web.ModuleMvc2/wwwroot/Scripts/foo.js
+++
@@ -1,3 +0,0 @@
-(function () {
-    console.log('hello from ModuleMvc2 foo.js');
-})();
